dfd34f560c17f0580e54ab46a73d2ece25bd9ca9
M-Jakes <jakesmatyas@gmail.com>
2025-03-17 15:33:22 +0100

    kočička

--- storage.py
+++ storage.py
@@ -4,14 +4,31 @@ products = [
     ["tričko", 1],
 ]
 
+products2 = [
+    {
+        "name": "Audi",
+        "price": 50
+    },
+    {
+        "price": 30,
+        "name": "BMW",
+    }
+]
+
 def print_products():
     for product in products:
-        print(f"Název produktu: {product[0]}, cena: {product[1]}$")
+        print(f"Název produktu: {product[0]}, cena: {product[1]}$d")
+    for product in products2:
+        print(f"Název produktu: {product['name']}, cena: {product['price']}$")
 
 def add_product():
     product_name = input("Název produktu:")
     product_price = input("Název cenu:")
     product = [product_name, product_price]
+    product2 = {
+        'name': product_name,
+        'price': product_price
+    }
 
     products.append(product)
 
